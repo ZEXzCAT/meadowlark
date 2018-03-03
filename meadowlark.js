bba5e6777210801ad66f204e3576bc9126942343
ZEX <zexio1998@gmail.com>
2018-03-03 23:54:22 +0200

    meadwlark second commit

--- meadowlark.js
+++ meadowlark.js
@@ -7,6 +7,10 @@ var fortunes = [
   "Тебя ждет приятный сюрприз.",
   "Будь проще везде, где только можно.",
 ];
+var tours = [
+{ id: 0, name: 'Река Худ', price: 99.99 },
+{ id: 1, name: 'Орегон Коуст', price: 149.95 },
+];
 // Установка механизма представления handlebars
 var handlebars = require('express-handlebars').create({
   defaultLayout: 'main'
@@ -28,6 +32,42 @@ app.get('/about', function(req, res) {
   });
 });
 
+app.get('/headers', function(req, res) {
+  res.set('Content-Type', 'text/plain');
+  var s = '';
+  for (var name in req.headers)
+    s += name + ': ' + req.headers[name] + '\n';
+  res.send(s);
+});
+
+app.get('/api/tours', function(req, res) {
+  var toursXml = '<?xml version="1.0"?><tours>' +
+    products.map(function(p) {
+      return '<tour price="' + p.price +
+        '" id="' + p.id + '">' + p.name + '</tour>';
+    }).join('') + '</tours>';
+  var toursText = tours.map(function(p) {
+    return p.id + ': ' + p.name + ' (' + p.price + ')';
+  }).join('\n');
+  res.format({
+    'application/json': function() {
+      res.json(tours);
+    },
+    'application/xml': function() {
+      res.type('application/xml');
+      res.send(toursXml);
+    },
+    'text/xml': function() {
+      res.type('text/xml');
+      res.send(toursXml);
+    },
+    'text/plain': function() {
+      res.type('text/plain');
+      res.send(toursXml);
+    }
+  });
+});
+
 // Обобщенный обработчик 404 (промежуточное ПО)
 app.use(function(req, res, next) {
   res.status(404);
@@ -41,7 +81,9 @@ app.use(function(err, req, res, next) {
   res.render('500');
 });
 
+app.disable('x-powered-by');app.disable('x-powered-by');
+
 app.listen(app.get('port'), function() {
-  console.log('Express запущен на http://localhost/:' +
+  console.log('Express запущен на http://localhost:' +
     app.get('port') + '; нажмите Ctrl+C для завершения.');
 });
